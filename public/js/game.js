69f42aeed21ce517b563592abd29f492a6c953c2
Aleix Diaz <aleixdiazbaggerman@gmail.com>
2020-06-09 14:38:49 +0200

    Create add token on click

--- public/js/game.js
+++ public/js/game.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
     const roomName = document.getElementById('roomName').innerHTML
 
     //Sockets connections
-    let socket = io()
+    /*let socket = io()
     socket.emit('joinGame',  {
         userName: userName,
         roomName: roomName
@@ -13,9 +13,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
 
     socket.on('joinedRoom', (msg) => {
         document.getElementById('players').innerHTML = 'Players joined: 2/2'
-    })
+    })*/
 
 
     //Game tokens
-
+    $(".token-box").on('click', function(event) {
+        const target = event.target
+        if ($(target).hasClass('token')) return
+        $(target).removeClass('token-box')
+        $(target).addClass('token')
+    })
 })
